c37d12efcc6ee57a6e461b0667183e4f8a8f93a4
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-11-22 12:57:13 +0300

    byte deger

--- learn/Program.cs
+++ learn/Program.cs
@@ -1,4 +1,23 @@
-﻿Console.WriteLine("Hello,  merhaba World!");
+﻿using System;
+namespace learn
+{
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            byte birinci = 5;
+            byte maxdeger = byte.MaxValue;
+            byte mindeger = byte.MinValue;
+
+            Console.WriteLine(birinci);
+            Console.WriteLine(maxdeger); //255
+            Console.WriteLine(mindeger); //0
 
 
-Console.ReadLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}
