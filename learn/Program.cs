046cc2e8f25a3377ecc441c9bcdebd46a86e4bea
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-11-22 14:14:20 +0300

    eşittir öperatörü

--- learn/Program.cs
+++ learn/Program.cs
@@ -7,11 +7,14 @@ namespace operatorler
         static void Main(string[] args)
 
         {
-            // atama operatörü = 
+            // eşittir operatörü ==
             int sayi1 = 10;
-            
-            Console.WriteLine(sayi1);//
+            int sayi2 = 20;
+            bool kontrol = sayi1 == sayi2; // eşit mi kontrol eder eşitese True değilse False döndürür
            
+                     
+            Console.WriteLine(kontrol);
+            
             
             Console.ReadLine();
 
